254caa8ecdbf8ef63c8534eb504c5a95b4c62390
aggreyeb <aggreyeb@shaw.ca>
2016-02-02 07:51:46 -0700

    Implementing Application Listener

--- OTS/src/java/OTS/Servlets/ApplicationListener.java
+++ OTS/src/java/OTS/Servlets/ApplicationListener.java
@@ -1,3 +1,5 @@
+package OTS.Servlets;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,6 +7,7 @@
  */
 
 
+import App.NewHibernateUtil;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -23,7 +26,10 @@ public class ApplicationListener implements ServletContextListener {
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-         System.out.println("ServletContextListener contextDestroyed");
+         //System.out.println("ServletContextListener contextDestroyed");
+      if( NewHibernateUtil.getSessionFactory()!=null){
+          NewHibernateUtil.getSessionFactory().close();
+      }
     }
     
 }
